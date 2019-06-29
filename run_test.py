1fcd7e553799c3933c2e3b76c41a21d1ce989d2b
yvesgit <yvesborenstein@hotmail.com>
2019-06-29 16:31:37 +0800

    Update run_test.py

--- run_test.py
+++ run_test.py
@@ -6,6 +6,8 @@ class TestRun(unittest.TestCase):
 
     def test_execute(self):
       self.assertEqual(2, run.execute(1))
+    def test_expect_false(self):
+      self.assertEqual(2, run.execute(2))
 
 if __name__ == '__main__':
     unittest.main()
